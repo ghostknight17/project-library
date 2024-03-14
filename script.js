da8750b8b19f2ab7884f9fd30d7d256a36bee3d3
Lucas <125161227+ghostknight17@users.noreply.github.com>
2024-03-14 03:24:03 -0300

    add a function that generates random books with filler text

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-const myLibrary = []; //array declaration
-
-book1 = new Book('cacaco', 'maca quiño', 362);
-
-//create an object on the index 0 of the array
-
-myLibrary[0] = book1;
+//array declaration
+const myLibrary = [];
 
 //object constructor
-
 function Book(title, author, pages) {
   this.title = title;
   this.author = author;
@@ -15,8 +9,29 @@ function Book(title, author, pages) {
 }
 
 //this function makes a new book and adds it to the library
-
 function addBookToLibrary(title, author, pages) {
   const book = new Book(title, author, pages);
   myLibrary.push(book);
+}
+
+//this function adds random books to the array
+function addRandomBook() {
+  const randomTitle = makeString(7);
+  const randomAuthor = makeString(7);
+  const randomPages = Math.floor(Math.random() * 1000);
+  addBookToLibrary(randomTitle, randomAuthor, randomPages);
+}
+
+//this taken from internet function generates a random book
+function makeString(length) {
+  let result = '';
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
 }
