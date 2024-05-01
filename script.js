62fb313dc3c3870a48dc2940053fbfca85c51acf
ghostknight17 <125161227+ghostknight17@users.noreply.github.com>
2024-04-30 21:55:46 -0300

    Make input option for the user to add books
    On the HTML file there is a new button that when interact with opens a modal. On the modal there is a form.
    The script now has a function that adds a new book object to the library array, using the data provided by the user on the form.

--- script.js
+++ script.js
@@ -1,46 +1,23 @@
 //array declaration
 const myLibrary = [];
-
-document.getElementById('new-book-button').onclick = function () {
-  const newBookDialog = document.querySelector('#new-book-dialog');
-  newBookDialog.show();
-};
+//assigning modal value to newBookModal variable
+const newBookModal = document.querySelector('#new-book-modal');
 
 //object constructor
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.read = read;
 }
 
 //this function makes a new book and adds it to the library
-function addBookToLibrary(title, author, pages) {
-  const book = new Book(title, author, pages);
+function addBookToLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
 
-//this function adds random books to the array
-function addRandomBook() {
-  const randomTitle = makeString(7);
-  const randomAuthor = makeString(7);
-  const randomPages = Math.floor(Math.random() * 1000);
-  addBookToLibrary(randomTitle, randomAuthor, randomPages);
-}
-
-//this taken from internet function generates a random book
-function makeString(length) {
-  let result = '';
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
-  return result;
-}
-
+//this function loops through the books and show them on the console
 function libraryLoop() {
   for (let i = 0; i < myLibrary.length; i++) {
     console.log(
@@ -48,3 +25,38 @@ function libraryLoop() {
     );
   }
 }
+
+//this function makes the modal appears when clicked
+document.getElementById('new-book-button').onclick = function () {
+  newBookModal.showModal();
+};
+//this function adds a book with the user inputs as its properties and close the modal
+document.getElementById('add-button').onclick = function () {
+  title = document.getElementById('title').value;
+  author = document.getElementById('author').value;
+  pages = document.getElementById('pages').value;
+  read = document.getElementById('read').checked;
+  addBookToLibrary(title, author, pages, read);
+  newBookModal.close();
+};
+
+// //this function adds random books to the array
+// function addRandomBook() {
+//   const randomTitle = makeString(7);
+//   const randomAuthor = makeString(7);
+//   const randomPages = Math.floor(Math.random() * 1000);
+//   addBookToLibrary(randomTitle, randomAuthor, randomPages);
+// }
+
+// function makeString(length) {
+//   let result = '';
+//   const characters =
+//     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//   const charactersLength = characters.length;
+//   let counter = 0;
+//   while (counter < length) {
+//     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//     counter += 1;
+//   }
+//   return result;
+// }
