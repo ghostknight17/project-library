f4b74e9c1fabd0e4baf14c6c328e823e9ed84917
ghostknight17 <125161227+ghostknight17@users.noreply.github.com>
2024-05-04 16:37:06 -0300

    Add functionality for remove books from library and change read status
    On the script side of the app, the displayBooks() function was updated for removing books
    and also a option to change the book objects property from the display.
    
    Now editing the array from the front end its totally doable.

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(book);
 }
 
+// this function purpose is to display each book object inside the library array
+// into a card on a shelf div inside the html
 function displayBooks() {
   const bookShelf = document.getElementById('shelf');
 
@@ -29,8 +31,20 @@ function displayBooks() {
     bookShelf.removeChild(bookShelf.firstChild);
   }
 
-  myLibrary.forEach((book) => {
+  // this method execute a function once per every book
+  // a div is created 'bookCard' and according a condition
+  // ('read' status) the content is decided
+  // besides, a button to change that condition is also created
+  // then another button is created with the purpose of remove books
+  // some attributes and the button text are set
+  // then all created elements are append
+  myLibrary.forEach((book, index) => {
+    const readButton = document.createElement('button');
+    readButton.setAttribute('class', 'read-button');
+    readButton.setAttribute('data-index', `${index}`);
+
     const bookCard = document.createElement('div');
+
     if (book.read) {
       bookCard.innerHTML = `
         <p>${book.title}</p>
@@ -38,6 +52,7 @@ function displayBooks() {
         <p>${book.pages}</p>
         <p>You have already read it.</p>
       `;
+      readButton.textContent = 'mark as unread';
     } else {
       bookCard.innerHTML = `
         <p>${book.title}</p>
@@ -45,8 +60,41 @@ function displayBooks() {
         <p>${book.pages}</p>
         <p>You haven't read it yet.</p>
       `;
+      readButton.textContent = 'mark as read';
     }
+
+    const cardButton = document.createElement('button');
+
+    cardButton.setAttribute('class', 'remove-button');
+    cardButton.setAttribute('data-index', `${index}`);
+
+    cardButton.textContent = 'remove';
+
+    bookCard.appendChild(cardButton);
+    bookCard.appendChild(readButton);
     bookShelf.appendChild(bookCard);
+  });
+  // here an event listener is added to the remove button, the index attribute
+  // is read and then that index is remove from the array and the display
+  document.querySelectorAll('.remove-button').forEach((button) => {
+    button.addEventListener('click', function () {
+      const cardIndex = button.getAttribute('data-index');
+      myLibrary.splice(cardIndex, 1);
+      displayBooks();
+    });
+  });
+  // here an event listener is added to the read button, the index attribute
+  // is read and then that array index's read property is change
+  document.querySelectorAll('.read-button').forEach((button) => {
+    button.addEventListener('click', function () {
+      const cardIndex = button.getAttribute('data-index');
+      if (myLibrary[cardIndex].read) {
+        myLibrary[cardIndex].read = false;
+      } else {
+        myLibrary[cardIndex].read = true;
+      }
+      displayBooks();
+    });
   });
 }
 
@@ -54,6 +102,7 @@ function displayBooks() {
 document.getElementById('new-book-button').onclick = function () {
   newBookModal.showModal();
 };
+
 // this function adds a book object with the user inputs
 // as its properties to the Library array and close the modal
 document.getElementById('add-button').onclick = function () {
@@ -65,10 +114,3 @@ document.getElementById('add-button').onclick = function () {
   newBookModal.close();
   displayBooks();
 };
-
-// this provisional code adds some books examples to work with
-addBookToLibrary('example of title', 'example of author', 45);
-addBookToLibrary('example of title 2', 'example of author', 87, true);
-addBookToLibrary('example of title 3', 'example of author', 62);
-
-displayBooks();
